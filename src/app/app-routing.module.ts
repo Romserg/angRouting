d29ef8c9361c927bafeb3bb59ce5646d9cdcb59b
Romserg <serg.romankov@gmail.com>
2021-03-24 23:26:53 +0200

    Add lazy loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './home/welcome.component';
 import { PageNotFoundComponent } from './page-not-found.component';
-
-const ROUTES = [
-  { path: '', redirectTo: 'welcome', pathMatch: 'full' },
-  { path: 'welcome', component: WelcomeComponent },
-  { path: '**', component: PageNotFoundComponent },
-];
+import { AuthGuard } from './user/auth.guard';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(ROUTES),
+    RouterModule.forRoot([
+      { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+      { path: 'welcome', component: WelcomeComponent },
+      {
+        path: 'products',
+        canLoad: [AuthGuard],
+        loadChildren: () =>
+          import('./products/product.module').then(m => m.ProductModule),
+      },
+      { path: '**', component: PageNotFoundComponent },
+    ]),
   ],
   exports: [RouterModule],
 })
--- src/app/user/auth.guard.ts
+++ src/app/user/auth.guard.ts
@@ -1,24 +1,40 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  CanLoad,
+  Route,
+  Router,
+  RouterStateSnapshot,
+  UrlTree,
+} from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(private authService: AuthService, private router: Router) {
   }
+
+  // Load module every time, even if not logged in
   canActivate(
     route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    state: RouterStateSnapshot,
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     return this.checkLoggedIn(state.url);
   }
 
+  // Load module in case if user logged in, otherwise not loading
+  canLoad(route: Route): Observable<boolean> | Promise<boolean> | boolean {
+    return this.checkLoggedIn(route.path);
+  }
+
   checkLoggedIn(url: string): boolean {
-    if (this. authService.isLoggedIn) {
-      return true
+    if (this.authService.isLoggedIn) {
+      return true;
     }
     this.authService.redirectUrl = url;
     this.router.navigate(['/login']);
